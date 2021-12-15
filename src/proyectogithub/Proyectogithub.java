42d0bfc58bdd3858bea6c6217ef2d992b6c6ad25
omarSuarezRodriguez <omar.suarez.rod@gmail.com>
2021-12-15 03:09:41 -0500

    Se agregra un hola mundo

--- src/proyectogithub/Proyectogithub.java
+++ src/proyectogithub/Proyectogithub.java
@@ -18,6 +18,7 @@ public class Proyectogithub {
         // TODO code application logic here
         
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo 2");
     }
     
 }
